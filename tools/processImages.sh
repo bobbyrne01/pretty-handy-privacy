80fe02b54db7aec1ad8abb51945c8795d8d84272
Robert Byrne <bobbyrne01@gmail.com>
2018-12-19 13:08:18 +0000

    Initial commit

--- tools/processImages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -p build
-convert images/originalAppIcon.png -bordercolor none -border 2x2 -background Black -alpha background -channel A -blur 0x10 -level 0,50% build/icon.png
-cp images/originalTrayIcon.png trayIcon.png
